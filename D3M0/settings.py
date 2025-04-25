c42e82063432143f3e670ee684d1674939d88d3d
ArifZaimah <ahmadarif.az@gmail.com>
2025-04-25 10:46:19 +0800

    add dashboard

--- D3M0/settings.py
+++ D3M0/settings.py
@@ -9,12 +9,11 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -56,7 +55,7 @@ ROOT_URLCONF = 'D3M0.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'your_app', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -111,6 +110,9 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+APPEND_SLASH = True
+
 
 
 # Static files (CSS, JavaScript, Images)
--- your_app/views.py
+++ your_app/views.py
@@ -1,9 +1,25 @@
-"""
+
 from django.shortcuts import render
+from django.shortcuts import redirect
 
 # Create your views here.
+def dashboard(request):
+    context = {
+        "user_count" : 42,
+        "sales" : 999.99,
+    }
+    return render(request, "dashboard/index.html", context)
+
+def home(request):
+    return redirect('dashboard')  # uses the name='dashboard' in urls.py
 """
 from django.http import HttpResponse
 
 def home(request):
     return HttpResponse('Hello, World!')
+
+from django.shortcuts import render
+
+def dashboard(request):
+    return render(request, 'dashboard/index.html')
+"""
